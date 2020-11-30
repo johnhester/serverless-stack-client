4456adbeb7edc58d61521ace882ecd943eec310a
John Hester <hesterusmaximus@gmail.com>
2020-11-29 21:29:20 -0600

    configured aws amplify

--- src/index.js
+++ src/index.js
@@ -4,6 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
+// Initialize AWS Amplify
+// Auth refers to Cognito
+// Storage refers to S3
+// API refers to API Gateway
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
