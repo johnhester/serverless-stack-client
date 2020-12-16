5d00fe02336ee4d4e8d865b1bb1e72a5f472b1fb
John Hester <hesterusmaximus@gmail.com>
2020-12-16 07:17:39 -0600

    commit

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
 
-const isLocal = false;
+const isLocal = process.env.NODE_ENV === "development";
 
 export function initSentry() {
     if (isLocal) {
